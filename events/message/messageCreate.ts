7c7c98c13d12a2186876aa748ae3228e73ef8652
Vijay <vijayvermakvp@gmail.com>
2024-09-07 11:58:07 +0530

    Improvements

--- events/message/messageCreate.ts
+++ events/message/messageCreate.ts
@@ -17,80 +17,86 @@ module.exports = {
 
     // Update User Experience and Level
 
-    try {
-      const guild = await prisma.guild.findUnique({
-        where: { guildId: message.guild?.id },
-      });
-
-      if (!guild) return;
-
-      const guildUser = await prisma.guildUser.findUnique({
-        where: {
-          userId_guildId: {
-            userId: message.author.id,
-            guildId: message.guildId!,
-          },
-        },
-      });
-
-      if (!guildUser) {
-        await prisma.guildUser.create({
-          data: {
-            userId: message.author.id,
-            experience: 1,
-            guildId: guild.guildId,
-          },
+    if (!message.channel.isThread()) {
+      try {
+        const guild = await prisma.guild.findUnique({
+          where: { guildId: message.guild?.id },
         });
-      } else {
-        const exp = getRandomNumberInRange(1, 5);
-        const updatedExp = guildUser.experience + exp;
 
-        const levelUp =
-          updatedExp >= expRequiredToReachNextLevel(guildUser.level);
+        if (!guild) return;
 
-        // Update User - Add Experience
-
-        await prisma.guildUser.update({
+        const guildUser = await prisma.guildUser.findUnique({
           where: {
             userId_guildId: {
               userId: message.author.id,
               guildId: message.guildId!,
             },
           },
-          data: {
-            experience: levelUp ? 1 : updatedExp,
-            level: levelUp ? guildUser.level + 1 : guildUser.level,
-          },
         });
 
-        if (levelUp) {
-          let levelRole = await prisma.levelRole.findUnique({
+        if (!guildUser) {
+          await prisma.guildUser.create({
+            data: {
+              userId: message.author.id,
+              experience: 1,
+              guildId: guild.guildId,
+            },
+          });
+        } else {
+          const exp = getRandomNumberInRange(1, 3);
+          const updatedExp = guildUser.experience + exp;
+
+          const levelUp =
+            updatedExp >= expRequiredToReachNextLevel(guildUser.level);
+
+          // Update User - Add Experience
+
+          await prisma.guildUser.update({
             where: {
-              guildId_level: {
-                level: guildUser.level + 1,
+              userId_guildId: {
+                userId: message.author.id,
                 guildId: message.guildId!,
               },
             },
+            data: {
+              experience: levelUp ? 1 : updatedExp,
+              level: levelUp ? guildUser.level + 1 : guildUser.level,
+            },
           });
 
-          if (levelRole) {
-            const role = await message.guild.roles.fetch(levelRole?.roleId);
-            if (role)
-              assignRole(message.guild, message.author.id, levelRole.roleId);
-            else levelRole = null;
-          }
+          if (levelUp) {
+            let levelRole = await prisma.levelRole.findUnique({
+              where: {
+                guildId_level: {
+                  level: guildUser.level + 1,
+                  guildId: message.guildId!,
+                },
+              },
+            });
 
-          if (guild.levelUpChannelId) {
-            const channel = await message.guild?.channels.fetch(
-              guild.levelUpChannelId
-            );
-            const messageContent = levelUpMessage(guildUser, guild, levelRole);
-            if (channel?.isTextBased()) await channel.send(messageContent);
+            if (levelRole) {
+              const role = await message.guild.roles.fetch(levelRole?.roleId);
+              if (role)
+                assignRole(message.guild, message.author.id, levelRole.roleId);
+              else levelRole = null;
+            }
+
+            if (guild.levelUpChannelId) {
+              const channel = await message.guild?.channels.fetch(
+                guild.levelUpChannelId
+              );
+              const messageContent = levelUpMessage(
+                guildUser,
+                guild,
+                levelRole
+              );
+              if (channel?.isTextBased()) await channel.send(messageContent);
+            }
           }
         }
+      } catch (error) {
+        console.log("Error - Updating User Experience", error);
       }
-    } catch (error) {
-      console.log("Error - Updating User Experience", error);
     }
 
     // Text Commands Execution
